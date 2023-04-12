9d9f7fca57b46d21f5e7ff22109461a85c2746d0
sandymaramma <edesandraoge@gmail.com>
2023-04-12 03:07:26 -0700

    multi language

---
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+//  a script that prints “JavaScript is amazing”
+
+// a constant variable: myVar, and a string value “JavaScript is amazing” assigned.
+const myVar = 'JavaScript is amazing';
+
+// this creates a function console.log(), used to print the value of myVar.
+console.log(myVar);
---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// this script prints 3 lines
+
+// initializing 3 variables
+const firstLine = 'C is fun';
+const secondLine = 'Python is cool';
+const thirdLine = 'JavaScript is amazing';
+
+// using function console.log() to print 3 lines
+console.log(firstLine);
+console.log(secondLine);
+console.log(thirdLine);
