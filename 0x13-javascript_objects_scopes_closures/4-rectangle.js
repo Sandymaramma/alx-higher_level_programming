c9b7691b51b9358992942b8477b70a86bf364b64
sandymaramma <edesandraoge@gmail.com>
2023-04-12 14:55:52 -0700

    rectangle4

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,34 @@
+#!/usr/bin/node
+// class Rectangle that defines a rectangle
+
+class Rectangle {
+  constructor (w, h) {
+    if ((w > 0) && (h > 0)) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let r = '';
+      for (let j = 0; j < this.width; j++) {
+        r += 'X';
+      }
+      console.log(r);
+    }
+  }
+
+  rotate () {
+    const aux = this.width;
+    this.width = this.height;
+    this.height = aux;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+
+module.exports = Rectangle;
