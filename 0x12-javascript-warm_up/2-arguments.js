efab20c9dc0e1690945d1a1cd86d4ac597ab1ad3
sandymaramma <edesandraoge@gmail.com>
2023-04-12 06:28:13 -0700

    arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,15 +1,15 @@
 #!/usr/bin/node
 // script that prints message depending on number of arguments passed
 
-const argsCount = process.argv.length - 2;
+const argsCount = process.argv;
 
 // if...else statements were used to check number of arguments passed
 // console.log()function was used to print all messages
 
-if (argsCount === 0) {
+if (argsCount.length === 0) {
   console.log('No argument');
-} else if (argsCount === 1) {
+} else if (argsCount.length === 1) {
   console.log('Argument found');
 } else {
-  console.log('Argument found');
+  console.log('Arguments found');
 }
