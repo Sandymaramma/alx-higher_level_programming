36d2c7b2a39e3f6e4b342dd97b8bc568d986138d
sandymaramma <edesandraoge@gmail.com>
2023-04-13 13:27:45 -0700

    factor index

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,13 +1,8 @@
-#!/usr/bin/node
+const list = require('./100-data').list;
 
-// Import the `list` array from the `100-data.js` file
-const { list } = require('./100-data');
+const multipliedList = list.map((value, index) => {
+  return value * index;
+});
 
-// Log the original `list` array
-console.log('Original list:', list);
-
-// Create a new array by multiplying each element of the original array by its index
-const multipliedList = list.map((number, index) => number * index);
-
-// Log the new array
-console.log('Multiplied list:', multipliedList);
+console.log(`Original list: ${list}`);
+console.log(`Multiplied list: ${multipliedList}`);
