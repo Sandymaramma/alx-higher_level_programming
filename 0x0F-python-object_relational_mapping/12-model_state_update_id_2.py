8e42a021998c03695138cd9a8b29d1840329903f
sandymaramma <edesandraoge@gmail.com>
2023-04-18 01:16:18 -0700

    update a state

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,20 +1,41 @@
 #!/usr/bin/python3
-"""Script that changes the name of a State
-object from the database hbtn_0e_6_usa"""
-import sys
-from sqlalchemy import (create_engine)
-from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
 
+"""
+a script that changes the name of a State object from the database
+hbtn_0e_6_usa
+"""
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from model_state import State, Base
+import sys
 
 if __name__ == "__main__":
-    eng = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                        .format(sys.argv[1], sys.argv[2],
-                                sys.argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(eng)
-    Session = sessionmaker(bind=eng)
+    """
+    Create the engine
+    """
+    url = ('mysql://{}:{}@localhost:3306/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]))
+
+    engine = create_engine(url)
+
+    """
+    Create a session
+    """
+
+    Session = sessionmaker(bind=engine)
     session = Session()
-    res = session.query(State).filter(State.id == 2) \
-                 .update({"name": "New Mexico"}, synchronize_session='fetch')
-    session.commit()
-    session.close()
+
+    """
+    Write the sql query to be executed below
+    """
+
+    """
+    update the required state
+    """
+    user = session.query(State).filter(State.id == 2).first()
+    if user:
+        user.name = 'New Mexico'
+        session.commit()
+        session.close()
