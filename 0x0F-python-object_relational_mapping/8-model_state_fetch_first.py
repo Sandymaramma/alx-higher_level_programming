fdd6efe0e25493a9696186dd45be2b7ef4b42b34
sandymaramma <edesandraoge@gmail.com>
2023-04-18 00:48:52 -0700

    first state

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,21 +1,34 @@
 #!/usr/bin/python3
-"""Script that prints the first State object from the database hbtn_0e_6_usa"""
-import sys
-from sqlalchemy import (create_engine)
-from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
 
+"""
+a script that prints the first State object from the database hbtn_0e_6_usa
+"""
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+import sys
 
 if __name__ == "__main__":
-    eng = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                        .format(sys.argv[1], sys.argv[2],
-                                sys.argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(eng)
-    Session = sessionmaker(bind=eng)
+    """Create the engine
+    """
+    url = ('mysql://{}:{}@localhost:3306/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]))
+    engine = create_engine(url)
+
+    """
+    Create a session to interaact with the database engine
+    """
+    Session = sessionmaker(bind=engine)
     session = Session()
-    res = session.query(State).first()
-    if res:
-        print(f"{res.id}: {res.name}")
+
+    """
+    Write the sql query to be executed
+    """
+
+    User = session.query(State).order_by(State.id).first()
+    if User:
+        print(f"{User.id}: {User.name}")
     else:
         print("Nothing")
-    session.close()
