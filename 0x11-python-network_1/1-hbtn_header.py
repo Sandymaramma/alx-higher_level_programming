9ea37d82b8b4d1981c4553f06e50ecda17ee3071
sandymaramma <edesandraoge@gmail.com>
2023-04-28 17:51:32 -0700

    post email

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-""" Python script that takes in a URL, sends a request to the URL and displays the value """
+""" a Python script that takes in a URL, sends a request to the URL and
+displays the value of the X-Request-Id variable found in the header of
+the response """
+import urllib.request
+import sys
 
 if __name__ == "__main__":
-    import urllib.request
-    import urllib.error
-    import sys
-
-    try:
-        with urllib.request.urlopen(sys.argv[1]) as r:
-            print(r.getheader('X-Request-Id'))
-    except (urllib.error.URLError, IndexError) as e:
-        print(e)
+    url = sys.argv[1]
+    R = urllib.request.Request(url)
+    with urllib.request.urlopen(R) as response:
+        R_id = response.headers.get("X-Request-Id")
+        print(R_id)
